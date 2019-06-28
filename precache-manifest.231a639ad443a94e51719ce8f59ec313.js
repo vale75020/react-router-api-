6d63e5ed09711c8d014b243acfa1b53eeed82dd1
vale75020 <valentinosuma@hotmail.com>
2019-06-28 13:19:11 +0200

    Updates

---
+++ precache-manifest.231a639ad443a94e51719ce8f59ec313.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc91f4291db965a77dd7c09b0c99f473",
+    "url": "/react-router-api/index.html"
+  },
+  {
+    "revision": "892e24418d2ef362abcc",
+    "url": "/react-router-api/static/css/main.66a0d831.chunk.css"
+  },
+  {
+    "revision": "7ddeded651437667509c",
+    "url": "/react-router-api/static/js/2.5df5668c.chunk.js"
+  },
+  {
+    "revision": "892e24418d2ef362abcc",
+    "url": "/react-router-api/static/js/main.36aef167.chunk.js"
+  },
+  {
+    "revision": "2b93fde44d91f959483a",
+    "url": "/react-router-api/static/js/runtime~main.61dc53bd.js"
+  }
+]);
